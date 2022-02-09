97b11816c25daae27a13cd6a057163430f2598c7
dawidrzuk <dawidrzukk@gmail.com>
2022-02-09 08:50:14 +0100

    wszystko dziala

--- app/src/main/java/com/example/dzialajacaaplikacja0902/MainActivity.kt
+++ app/src/main/java/com/example/dzialajacaaplikacja0902/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.dzialajacaaplikacja0902
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.dzialajacaaplikacja0902.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupAutoCompleteTextView()
+    }
+
+    private fun setupAutoCompleteTextView() {
+        val countries = resources.getStringArray(R.array.countries_array)
+        // Create the adapter and set it to the AutoCompleteTextView
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+        binding.autoCompleteTextView.setAdapter(adapter)
+
+        binding.btnSubmit.setOnClickListener {
+            val text = getString(R.string.entered_text) + " " + binding.autoCompleteTextView.text
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
